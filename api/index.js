59d84e7c25ca7fc4116af2dc287351a91063b8ca
Usama <usamamukhtar.work@gmail.com>
2025-02-14 16:40:01 +0500

    trying again after changes in backend's index.js

--- api/index.js
+++ api/index.js
@@ -3,22 +3,26 @@ import mongoose from "mongoose";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
-import postRoutes from './routes/post.route.js'
-import commentRoutes from './routes/comment.route.js'
-import path from 'path';
-import dotenv from 'dotenv';
-import cors from 'cors';
+import postRoutes from "./routes/post.route.js";
+import commentRoutes from "./routes/comment.route.js";
+import path from "path";
+import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
-const mongoUrl = process.env.MONGO_URI
+const mongoUrl = process.env.MONGO_URI;
 const run = async () => {
   try {
     await mongoose.connect(mongoUrl, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log(mongoUrl === "mongodb://localhost:27017/" ? "connected to local MongoDB!": "connected to Atlas MongoDB!");
+    console.log(
+      mongoUrl === "mongodb://localhost:27017/"
+        ? "connected to local MongoDB!"
+        : "connected to Atlas MongoDB!"
+    );
   } catch (error) {
     console.error("MongoDB Connection Error:", error);
     process.exit(1);
@@ -40,24 +44,25 @@ run().then(() => {
   });
 });
 
-app.use(cors({
-  origin: 'http://localhost:5173',  // Allow all origins (not recommended for production)
-  // origin: '*',  // Allow all origins (not recommended for production)
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Allow all origins (not recommended for production)
+    // origin: '*',  // Allow all origins (not recommended for production)
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
-app.use('/api/post', postRoutes)
-app.use('/api/comment', commentRoutes)
+app.use("/api/post", postRoutes);
+app.use("/api/comment", commentRoutes);
 
-app.use(express.static(path.join(_dirname, '/client/dist')));
+app.use(express.static("/client/dist"));
 
-
-app.get('*', (req, res) =>{
-    res.sendFile(path.join(_dirname, 'client', 'dist', 'index.html'))
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(_dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
